36c65e015163939adaccd5cf66f5c21fa04c6186
Abhishec177 <abhishek122k@gmail.com>
2022-02-20 22:35:58 +0530

    completed week-2 exercise

---
+++ exercises/week-2/voting-system/target/rls/debug/deps/voting_system-57e3ffd7f72b1cfa.d
@@ -0,0 +1,5 @@
+d:\Solana\solana-india-fellowship-abhishek\exercises\week-2\voting-system\target\rls\debug\deps\voting_system-57e3ffd7f72b1cfa.rmeta: src\main.rs
+
+d:\Solana\solana-india-fellowship-abhishek\exercises\week-2\voting-system\target\rls\debug\deps\voting_system-57e3ffd7f72b1cfa.d: src\main.rs
+
+src\main.rs:
---
+++ exercises/week-2/voting-system/target/rls/debug/deps/voting_system-b442ef7d1f750e1d.d
@@ -0,0 +1,5 @@
+d:\Solana\solana-india-fellowship-abhishek\exercises\week-2\voting-system\target\rls\debug\deps\voting_system-b442ef7d1f750e1d.rmeta: src\main.rs
+
+d:\Solana\solana-india-fellowship-abhishek\exercises\week-2\voting-system\target\rls\debug\deps\voting_system-b442ef7d1f750e1d.d: src\main.rs
+
+src\main.rs:
